ce88d7c84d63c38ef23d7f916edd8e1f6573ad63
AlanPires01 <alanpires@alu.ufc.br>
2024-09-26 14:37:03 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Route;
+
+Route::get("/",[LoginController::class,"index"])->name("login.index");
+
+Route::post("/login",[LoginController::class,"login"])->name("login.process");
